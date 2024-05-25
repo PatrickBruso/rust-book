b11fabc6bd99dfd8a32e8bdb570cfc3a7df01a49
Patrick Bruso <pkbruso@gmail.com>
2024-05-24 21:04:17 -0700

    Code first part of guessing game app

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+// import standard input/output library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    // initalize new mutable variable 'guess' as an empty string
+
+    io::stdin()
+        .read_line(&mut guess)  // read user input and store it to 'guess' variable via reference
+        .expect("Failed to read line"); // error checking
+
+    println!("You guessed: {guess}");
 }
