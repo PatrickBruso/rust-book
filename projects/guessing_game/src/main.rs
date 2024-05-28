851d7225361cabd11540a2d2f0faf3e675db59d5
Patrick Bruso <pkbruso@gmail.com>
2024-05-28 09:02:09 -0700

    implemented random number generator to generate random number

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,8 +1,15 @@
 use std::io;
 // import standard input/output library
+use rand::Rng;
+// import random number generator
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // create new variable 'secret_number' and set it to randon number between 1 and 100
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
